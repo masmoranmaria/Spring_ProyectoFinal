234556c85ed22dd6b8963c85494d48b9ccc8eb73
Maria <masmoranmaria@gmail.com>
2022-05-25 19:06:32 +0200

    Proyecto Productores

---
+++ Productores/src/main/java/com/Productores/ProductoresApplication.java
@@ -0,0 +1,13 @@
+package com.Productores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoresApplication.class, args);
+	}
+
+}
---
+++ Productores/src/test/java/com/Productores/ProductoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Productores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
